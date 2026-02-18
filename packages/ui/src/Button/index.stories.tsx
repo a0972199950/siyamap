1f1f99e8e3bc9a27eaf73f00acdd06fcb3015c29
John H <a0972199950@gmail.com>
2026-02-18 13:45:44 +0800

    add button

--- packages/ui/src/Button/index.stories.tsx
+++ packages/ui/src/Button/index.stories.tsx
@@ -3,12 +3,60 @@ import Button from "./index"
 
 const meta: Meta<typeof Button> = {
   component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: { type: "select" },
+      options: ["primary", "secondary"],
+      description: "按鈕類型"
+    },
+    color: {
+      control: { type: "select" },
+      options: [
+        "primary-500",
+        "primary-400",
+        "secondary-500",
+        "secondary-400",
+        "secondary-300",
+        "red",
+        "green",
+        "blue"
+      ],
+      description: "按鈕顏色"
+    },
+    rounded: {
+      control: { type: "select" },
+      options: ["none", "sm", "md", "lg", "full"],
+      description: "按鈕圓角"
+    },
+    size: {
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+      description: "按鈕大小"
+    },
+    className: {
+      control: { type: "text" },
+      description: "自定義 CSS 類名"
+    },
+    children: {
+      control: { type: "text" },
+      description: "按鈕內容"
+    }
+  }
 }
 
 export default meta
 
 export const Primary: StoryObj<typeof Button> = {
   args: {
-    children: "Hello Button",
+    children: "分享視野",
+    type: "primary"
+  },
+}
+
+export const Secondary: StoryObj<typeof Button> = {
+  args: {
+    children: "登入",
+    type: "secondary"
   },
 }
--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -1,8 +1,38 @@
 import React from "react"
+import clsx from 'clsx';
 import styles from "./index.module.css"
+import { UiColors } from "../types/common"
 
-const Button = ({ children }: { children: React.ReactNode }) => {
-  return <button className={styles.foo__bar}>{children}</button>
+interface Props {
+  type?: 'primary' | 'secondary'
+  color?: UiColors
+  rounded?: 'none' | 'sm' | 'md' | 'lg' | 'full'
+  size?: 'sm' | 'md' | 'lg'
+  className?: string
+  children: React.ReactNode
+}
+
+const Button = (props: Props): JSX.Element => {
+  const { type = 'primary', color, rounded, size, className, children } = props
+
+  const classes = clsx(
+    styles['button'],
+    styles[`button--${type}`],
+    styles[`button--rounded-${rounded}`],
+    styles[`button--size-${size}`],
+    className
+  )
+
+  return (
+    <button
+      className={classes}
+      style={{
+        '--btn-custom-bg-color': `var(--color-ui-${color})`
+      } as React.CSSProperties}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
